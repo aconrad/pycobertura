fb70cc9f52bda3bbd12d8a2fc7d2f0a584318dea
Alex Conrad <alexandre@surveymonkey.com>
2014-12-03 09:03:05 +0000

    bump to v0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '0.0.2'
+version = '0.1.0'
 
 try:
     import pypandoc
