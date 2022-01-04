3b6e201ea65ae8db31c13ae38802e05a30567bc0
Michael Gross <leachimgross@gmail.com>
2022-01-04 12:45:28 +0100

    fix Python version specifiers in setup.py

--- setup.py
+++ setup.py
@@ -39,11 +39,11 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4"
+        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     entry_points={
         'console_scripts': [
