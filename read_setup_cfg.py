722d190e7f520d93a555766a718efd4b69ff5ba0
Michael Gross <leachimgross@gmail.com>
2022-01-08 18:51:58 +0100

    add setup.cfg parser

---
+++ read_setup_cfg.py
@@ -0,0 +1,21 @@
+import configparser
+import argparse
+
+config = configparser.ConfigParser()
+
+with open('setup.cfg') as f:
+    config.read_file(f)
+
+parser = argparse.ArgumentParser(description='Return setup.cfg metadata value for given metadata key')
+parser.add_argument('--section', metavar='section', nargs=1, help="section header")
+parser.add_argument('--key', metavar='key', nargs=1, help='metadata key')
+args = vars(parser.parse_args())
+
+section, key = args['section'], args['key']
+
+if type(section) == list:
+    section = section[0]
+if type(key) == list:
+    key = key[0]
+
+print(config[section][key])
--- release.sh
+++ release.sh
@@ -1,16 +1,13 @@
 # Before you release
-# - install pandoc on your system: apt-get install pandoc
-# - install pypandoc: pip install pypandoc
 # - install twine: pip install twine
-# - bump package version in setup.py
+# - bump package version in setup.cfg
 # - update the package version in the CHANGES file
 # - commit the changes to master and push
 
-# TODO: update with setup.cfg configparser read metadata
-PKG_NAME=$(python setup.py --name)
-PKG_VERSION=$(python setup.py --version)
+PKG_NAME=$(python read_setup_cfg.py --section metadata --key name)
+PKG_VERSION=$(python read_setup_cfg.py --section metadata --key version)
 
-git tag -am "release v${PKG_VERSION}" v${PKG_VERSION}
+git tag -am "release v${PKG_VERSION}" "v${PKG_VERSION}"
 git push --tags
-python setup.py sdist
-twine upload dist/${PKG_NAME}-${PKG_VERSION}.tar.gz
+python -m build
+twine upload dist/"${PKG_NAME}-${PKG_VERSION}*"
