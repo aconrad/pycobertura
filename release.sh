030202aace820c0d37c595872a7a5359ff91b2b8
Michael Gross <leachimgross@gmail.com>
2022-01-12 18:23:56 +0100

    use pkg_resources to extract library version

--- release.sh
+++ release.sh
@@ -4,8 +4,9 @@
 # - update the package version in the CHANGES file
 # - commit the changes to master and push
 
-PKG_NAME = $(python read_setup_cfg.py | head -n1)
-PKG_VERSION = $(python read_setup_cfg.py | tail -n1)
+PKG_NAME = pycobertura
+# from importlib import metadata is unfortunately only available from Python 3.8 onwards
+PKG_VERSION = python -c "from pkg_resources import get_distribution; print(get_distribution('pycobertura').version)"
 
 git tag -am "release v${PKG_VERSION}" "v${PKG_VERSION}"
 git push --tags
