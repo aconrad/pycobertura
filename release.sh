ed507174ba26c28455ff14b7cd20c2e23991ee0e
Alex Conrad <aconrad@grindfoundry.com>
2022-10-08 02:43:11 +0200

    Fix-up release script
    Since we change `setup.py` and `setup.cfg`, a few things were broken and we
    could only figure it out while going through the release process. Now that
    we've released 3.0.0 and went through the pain points, those updates are the
    fixes to make the next release more smooth.

--- release.sh
+++ release.sh
@@ -4,11 +4,14 @@
 # - update the package version in the CHANGES file
 # - commit the changes to master and push
 
+pip install .  # installs the new version after bump so the line PKG_VERSION gets the right version
+
 PKG_NAME=pycobertura
 # from importlib import metadata is unfortunately only available from Python 3.8 onwards
 PKG_VERSION=$(python -c "from pkg_resources import get_distribution; print(get_distribution('pycobertura').version)")
 
 git tag -am "release v${PKG_VERSION}" "v${PKG_VERSION}"
 git push --tags
+pip install build
 python -m build
-twine upload dist/"${PKG_NAME}-${PKG_VERSION}"{.tar.gz,.whl}
+twine upload dist/"${PKG_NAME}-${PKG_VERSION}"*{.tar.gz,.whl}
