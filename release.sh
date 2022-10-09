148035b8f13b9791807a100409101411d9f22f0a
Alex Conrad <aconrad@grindfoundry.com>
2022-10-09 11:36:44 +0200

    Store package version in file `__version__`

--- release.sh
+++ release.sh
@@ -1,14 +1,12 @@
 # Before you release
 # - install twine: pip install twine
-# - bump package version in setup.cfg
+# - bump package version in `__version__`
 # - update the package version in the CHANGES file
 # - commit the changes to master and push
 
-pip install .  # installs the new version after bump so the line PKG_VERSION gets the right version
-
 PKG_NAME=pycobertura
 # from importlib import metadata is unfortunately only available from Python 3.8 onwards
-PKG_VERSION=$(python -c "from pkg_resources import get_distribution; print(get_distribution('pycobertura').version)")
+PKG_VERSION=$(cat __version__)
 
 git tag -am "release v${PKG_VERSION}" "v${PKG_VERSION}"
 git push --tags
